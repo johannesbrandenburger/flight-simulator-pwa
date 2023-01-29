ad4e213c30a3a5be34fa090fe4763f42f021a17f
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2023-01-29 13:48:36 +0100

    traffic-logger

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ window.onload = () => {
     if ("serviceWorker" in navigator) {
         navigator.serviceWorker.register("sw.js");
     }
+
+    try {
+        // this is for logging only (can be removed in production)
+        fetch('https://traffic-logger.brandenburger.dev/api/create-log', {
+            method: 'POST',
+            body: JSON.stringify({ app: 'flight-simulator-pwa', userAgent: navigator.userAgent }),
+        })
+    } catch (error) { }
 };
