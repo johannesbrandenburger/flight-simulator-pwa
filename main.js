02c013b2c75f8d7dc030faa68a9a7f763ca73841
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2023-04-26 09:28:54 +0200

    rm traffic-logger

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ window.onload = () => {
     navigator.serviceWorker.register("/sw.js");
   }
 
-  try {
-    // this is for logging only (can be removed in production)
-    fetch('https://traffic-logger.brandenburger.dev/api/create-log', {
-      method: 'POST',
-      body: JSON.stringify({ app: 'flight-simulator-pwa-vite', userAgent: navigator.userAgent }),
-    })
-  } catch (error) { }
+  // try {
+  //   // this is for logging only (can be removed in production)
+  //   fetch('https://traffic-logger.brandenburger.dev/api/create-log', {
+  //     method: 'POST',
+  //     body: JSON.stringify({ app: 'flight-simulator-pwa-vite', userAgent: navigator.userAgent }),
+  //   })
+  // } catch (error) { }
 
 };
 
